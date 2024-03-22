737d8e654c689112735948888c94b27aee977520
kouamdo <kouamdoux@gmail.com>
2024-03-22 12:10:58 +0100

    Update Category and attrib link

--- parent/ressources-jpa/document-jpa/src/main/java/cmr/notep/dao/CategoryEntity.java
+++ parent/ressources-jpa/document-jpa/src/main/java/cmr/notep/dao/CategoryEntity.java
@@ -1,29 +1,31 @@
 package cmr.notep.dao;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.dozer.Mapping;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "categorie")
+@Table(name = "category")
 public class CategoryEntity {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name = "id")
-    private String id;
+    @Column(name = "id_category", nullable = false)
+    private String id_category;
 
     @Column(name = "ordre")
     private String ordre;
 
     @Column(name = "libelle")
     private String libelle;
+
+    @ManyToMany(mappedBy = "AttribCategory")
+    @Mapping("attributs")
+    private List<AttributsEntity> CatAttrib ;
 }
