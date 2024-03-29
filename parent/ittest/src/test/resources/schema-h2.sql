1025ea14f4cf4f816e6f9294ace8e5e2b4603676
kouamdo <kouamdoux@gmail.com>
2024-03-29 18:33:06 +0100

    make update

--- parent/ittest/src/test/resources/schema-h2.sql
+++ parent/ittest/src/test/resources/schema-h2.sql
@@ -4,8 +4,8 @@ CREATE SCHEMA parcours AUTHORIZATION sa;
 CREATE TABLE IF NOT EXISTS attributs
 (
     id character varying  NOT NULL,
-    titre character varying  NOT NULL,
-    description character varying ,
+    titre VARCHAR(255)  NOT NULL,
+    description VARCHAR(255) ,
     etat boolean,
     datecreation date,
     datemodification date,
@@ -17,16 +17,16 @@ CREATE TABLE IF NOT EXISTS attributs
 
 CREATE TABLE IF NOT EXISTS constituer
 (
-    id_document character varying  NOT NULL,
+    id_document VARCHAR(255) NOT NULL,
     id_attribut character varying  NOT NULL,
     CONSTRAINT constituer_pkey PRIMARY KEY (id_document, id_attribut)
 );
 
 CREATE TABLE IF NOT EXISTS documents
 (
-    id character varying  NOT NULL,
+    id VARCHAR(255) NOT NULL,
     titre character varying  NOT NULL,
-    description character varying ,
+    description VARCHAR(255) ,
     etat boolean,
     datecreation date,
     datemodification date,
@@ -34,16 +34,16 @@ CREATE TABLE IF NOT EXISTS documents
 );
 
 CREATE TABLE category (
-    id_category INT PRIMARY KEY,
+    id_category character varying  PRIMARY KEY,
     ordre VARCHAR(255),
     libelle VARCHAR(255),
-    id_documents character varying  NOT NULL,
+    id_documents VARCHAR(255) NOT NULL,
     FOREIGN KEY (id_documents) REFERENCES documents(id)
 );
 
 CREATE TABLE associer (
-    id_attribut character varying  NOT NULL,
-    id_category INT NOT NULL,
+    id_attribut character varying NOT NULL,
+    id_category character varying NOT NULL,
     PRIMARY KEY (id_attribut, id_category),
     FOREIGN KEY (id_attribut) REFERENCES attributs(id),
     FOREIGN KEY (id_category) REFERENCES category(id_category)
