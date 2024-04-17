be8715a438a6f5b807809b88bfbfadc7acb14eb6
kouamdo <kouamdoux@gmail.com>
2024-04-17 11:22:43 +0200

    Add service for Attribut

--- parent/ittest/src/test/java/cmr/notep/commun/AbstractIttest.java
+++ parent/ittest/src/test/java/cmr/notep/commun/AbstractIttest.java
@@ -1,5 +1,6 @@
 package cmr.notep.commun;
 
+import cmr.notep.api.IAttributApi;
 import cmr.notep.api.IDocumentApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,4 +10,7 @@ public abstract class AbstractIttest {
 
   @Autowired
   protected   IDocumentApi documentService;
+
+  @Autowired
+  protected IAttributApi attributService ;
 }
