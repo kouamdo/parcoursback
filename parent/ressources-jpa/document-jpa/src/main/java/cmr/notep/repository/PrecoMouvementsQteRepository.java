f2c8901ceda4b626b211e909d1ac48951e4e2320
kouamdo <kouamdoux@gmail.com>
2024-05-09 06:15:46 +0200

    add PrecoMouvementsQteEntity repository

---
+++ parent/ressources-jpa/document-jpa/src/main/java/cmr/notep/repository/PrecoMouvementsQteRepository.java
@@ -0,0 +1,7 @@
+package cmr.notep.repository;
+
+import cmr.notep.dao.PrecoMouvementsQteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrecoMouvementsQteRepository extends JpaRepository<PrecoMouvementsQteEntity, String> {
+}
