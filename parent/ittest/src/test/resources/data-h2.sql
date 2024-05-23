4c386ffe8717a9de016d8406b6495445dc4b5d09
kouamdo <kouamdoux@gmail.com>
2024-05-23 07:28:05 +0200

    update jdd

--- parent/ittest/src/test/resources/data-h2.sql
+++ parent/ittest/src/test/resources/data-h2.sql
@@ -45,6 +45,77 @@ INSERT INTO associer (id_attribut, id_category, obligatoire, ordre) VALUES
 ('8', '2', false, 3);
 
 
+--------Mission jdd
+
+-- Insertion dans la table 'service'
+INSERT INTO service (id, libelle, description, codeuniq, etat, datecreation, datemodification) VALUES
+('1', 'Pharmacie', 'bien', '1', true, '2000-03-07', '1990-03-07'),
+('2', 'Laboratoire', 'bien', '2', true, '1990-08-06', '1990-03-07'),
+('3', 'Consultation', 'bien', '3', true, '1972-06-12', '1990-03-07');
+
+-- Insertion dans la table 'mission'
+INSERT INTO mission (id, libelle, description, etat, datecreation, datemodification, id_service) VALUES
+('1', 'Consultation', 'Consultation faite par une infirmière', true, '2000-03-07', '1990-03-07', '1'),
+('2', 'Consultation Spécialiste', 'Consultation faite par un médecin', true, '2000-03-07', '1990-03-07', '2'),
+('3', 'Prelevement Labo', 'Prélévement fait par laboratoire', true, '2000-03-07', '1990-03-07', '3'),
+('4', 'Encaissement', 'recu de paiement lié à une mission', true, '2000-03-07', '1990-03-07', '1'),
+('5', 'Resultat Labo', 'Communiquer les résultats du labo aux patients', true, '2000-03-07', '1990-03-07', '3'),
+('6', 'Hospitalisation', 'bon d''entrée et de sortie est une mission', true, '2000-03-07', '1990-03-07', '1');
+
+------attribut jdd
+
+INSERT INTO attributs (id, titre, description, etat, datecreation, datemodification, type, valeurpardefaut) VALUES
+('1', 'taille', 'taille de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.String', ''),
+('2', 'poids', 'poids de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.Double', ''),
+('3', 'sexe', 'sexe de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.Boolean', 'Homme, Femme, Autre'),
+('4', 'age', 'age de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.Double', ''),
+('5', 'allergies', 'allergies de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.String', ''),
+('6', 'teint', 'teint de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.String', ''),
+('7', 'Groupe sangin', 'Groupe sangin de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.Boolean', 'A, A+, A-, B, B+, B-, AB, AB+, AB-, O, O+, O-'),
+('8', 'cicatrice', 'cicatrice de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.Boolean', 'oui, non'),
+('9', 'date admission', 'date admission de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.Date', ''),
+('10', 'date decharge', 'date decharge', true, '2000-03-07', '1990-03-07', 'TypeTicket.Date', ''),
+('11', 'date prochain rendez-vous', 'date prochain rendez-vous de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.Date', ''),
+('12', 'aprobation du medecin', 'aprobation du medecin', true, '2000-03-07', '1990-03-07', 'TypeTicket.Boolean', 'oui, non'),
+('13', 'motif de la decharge', 'motif de la decharge', true, '2000-03-07', '1990-03-07', 'TypeTicket.Textarea', ''),
+('14', 'nom', 'nom de l''individu', true, '2000-03-07', '1990-03-07', 'TypeTicket.String', '');
+
+
+------precomouvement jdd
+
+-- Insertion dans la table precomouvements
+INSERT INTO precomouvements (id, libelle, etat, type) VALUES
+('1', 'rachat', true, 'neutre'),
+('2', 'vente', true, 'reduire'),
+('3', 'vente', true, 'reduire');
+
+-- Insertion dans la table precomouvementsqte
+INSERT INTO precomouvementsqte (id, qteMin, qteMax, montantMin, montantMax, id_precomouvements, id_ressources) VALUES
+('1', 10, 20, 1000, 7000, '1', '1'),
+('2', 30, 40, 100, 7000, '1', '2'),
+('3', 30, 40, 100, 7000, '1', '3'),
+('4', 30, 40, 100, 7000, '1', '4');
+
+-- Insertion dans la table ressources
+INSERT INTO ressources (id, libelle, etat, quantite, unite, prix, id_familles) VALUES
+('1', 'transfusion', true, 10, 'UnitesLitre', 1000, '4'),
+('2', 'néonat', true, 20, 'UnitesLitre', 2000, '3'),
+('3', 'pediatrie', true, 30, 'UnitesLitre', 3000, '4');
+
+-- Insertion dans la table familles
+INSERT INTO familles (id, libelle, description, etat, id_precomouvementsqte) VALUES
+('1', 'trans', 'sang', true, '1'),
+('2', 'néonat', 'nouveau-né', false, '1'),
+('3', 'pediatrie', 'enfant', true, '1'),
+('4', 'néonat', 'nouveau-né', false, '1');
+
+
+------service jdd
+
+
+
+
+
 --fin insertion
 
 
